23304d5e865789f57c7c1929e9bc076a2e67235a
ousseynou8 <ousseynoufofana15@gmail.com>
2021-10-26 12:19:55 +0200

    26/10

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import ChatBot from "../components/ChatBot";
+
+const Home = ({ sequence }) => (
+  <div>
+    <Head>
+      <title>Simple ChatBot example</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <ChatBot {...sequence} />
+  </div>
+);
+
+Home.getInitialProps = async (ctx) => {
+  const res = await fetch(
+    "https://run.mocky.io/v3/7479afe7-b3d4-4a1a-b583-b3f252884523"
+  );
+  const json = await res.json();
+  return { sequence: json };
+};
+
+export default Home;
